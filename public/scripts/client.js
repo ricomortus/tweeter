3a31278c3d9724e2fcb4ee23a89a65e3e3b054d9
Rico Chow <rico.ck.chow@gmail.com>
2023-11-15 11:33:21 -0500

    Fix createTweetElement and renderTweet using jQuery objects.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,53 +4,82 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//temporary object data
-
-let usrObj = {
+// Fake data taken from initial-tweets.json
+const data = [
+  {
     "user": {
       "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
     "created_at": 1461116232227
-  };
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-//create new tweet function
-const createTweetElement = function (obj) {
-  const date = new Date(obj.created_at);
-  const formattedDate = date.toLocaleString();
+const createTweetElement = function(tweet) {
+  // Convert Unix timestamp to a moment object and format it to a relative time
+  const formattedDate = moment(tweet.created_at).fromNow();
 
-  return `
+  // Create the tweet HTML using template literals
+  const tweetHTML = `
     <article class="tweet">
-    <header>
-      <div class="tweeter">
-       <img src=${obj.user.avatars} alt="user avatar">
-        <span>${obj.user.name}</span>
-      </div>
-      <span class="username">${obj.user.handle}</span>
-    </header>
-
-    <p>
-      ${obj.content.text}
-    </p>
-    
-    <footer>
-      <span>${formattedDate}</span>
-      <div class="tweet-functions">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-sharp fa-solid fa-repeat"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-   </article>
+      <header>
+        <div class="tweeter">
+          <img src="${tweet.user.avatars}" alt="user avatar">
+          <span>${tweet.user.name}</span>
+        </div>
+        <span class="username">${tweet.user.handle}</span>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <span>${formattedDate}</span>
+        <div class="tweet-functions">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-sharp fa-solid fa-repeat"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
   `;
+
+  // Convert the HTML string to a jQuery object and return it
+  return $(tweetHTML);
 };
 
-const $tweet = createTweetElement(usrObj);
 
+
+
+// Function definition outside of the document.ready
+
+const renderTweets = function(tweets) {
+  // Create an empty jQuery object
+  let $allTweets = $();
+
+  tweets.forEach(tweet => {
+    // Create a tweet element and add it to the jQuery object
+    $allTweets = $allTweets.add(createTweetElement(tweet));
+  });
+
+  // Append all tweets to the DOM at once
+  $('#tweets-container').append($allTweets);
+};
+
+
+// Document ready block
 $(document).ready(function() {
-  $('#tweets-container').append($tweet);
+  renderTweets(data); // Call the function when the DOM is ready
 });
